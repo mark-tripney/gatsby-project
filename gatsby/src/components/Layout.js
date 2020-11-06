1197617859614b01c1566744ed7b5487e8994b12
Mark Tripney <mrktrpny@gmail.com>
2020-11-06 14:52:53 +0100

    Add styling (and 'normalize'); destructure props

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Footer from './Footer';
 import Nav from './Nav';
+import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 
-export default function Layout(props) {
-  console.log(props);
-
+export default function Layout({ children }) {
   return (
     <div>
+      <GlobalStyles />
+      <Typography />
       <Nav />
-      {props.children}
+      {children}
       <Footer />
     </div>
   );
