c28c4f4cd27c012250d32763f3fbf11e92f21b58
Mark Tripney <hello@marktripney.com>
2020-11-06 17:21:57 +0100

    Create NavStyles styled component; add styling to same

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -1,9 +1,48 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+const NavStyles = styled.nav`
+  margin-bottom: 3rem;
+  ul {
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    list-style: none;
+    display: grid;
+    grid-template-columns: 1fr 1fr auto 1fr 1fr;
+    grid-gap: 2rem; // 20px
+    align-items: center;
+  }
+  li {
+    --rotate: -2deg;
+    transform: rotate(var(--rotate));
+    order: 1;
+    &:nth-child(1) {
+      --rotate: 1deg;
+    }
+    &:nth-child(2) {
+      --rotate: -2.5deg;
+    }
+    &:nth-child(4) {
+      --rotate: 2deg;
+    }
+    &:hover {
+      --rotate: 3deg;
+    }
+  }
+  a {
+    font-size: 3rem;
+    text-decoration: none;
+    &:hover {
+      color: var(--red); // from GlobalStyles.js
+    }
+  }
+`;
 
 export default function Nav() {
   return (
-    <nav>
+    <NavStyles>
       <ul>
         <li>
           <Link to="/">Hot now!</Link>
@@ -21,6 +60,6 @@ export default function Nav() {
           <Link to="/order">Order ahead!</Link>
         </li>
       </ul>
-    </nav>
+    </NavStyles>
   );
 }
