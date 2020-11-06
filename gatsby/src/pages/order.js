3b6aced0f63235ced1e63cfbe3877f90c0bba941
Mark Tripney <mrktrpny@gmail.com>
2020-11-06 14:53:38 +0100

    Apply Layout component

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Layout from '../components/Layout';
 
 export default function OrderPage() {
   return (
-    <Layout>
+    <>
       <p>📝 Hi, I'm the Order page!</p>
-    </Layout>
+    </>
   );
 }
--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Layout from '../components/Layout';
 
 export default function SlicemastersPage() {
   return (
-    <Layout>
+    <>
       <p>🧑‍🤝‍🧑 Hi, I'm the Slicemasters page!</p>
-    </Layout>
+    </>
   );
 }
